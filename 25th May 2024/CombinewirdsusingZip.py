013345af282e61fdfc125d8dc4aad91aa166b56c
Asik8 <motasembillah84@gmail.com>
2024-05-25 18:08:14 +0600

    Zip added

--- 25th May 2024/CombinewirdsusingZip.py
+++ 25th May 2024/CombinewirdsusingZip.py
@@ -1,5 +1,8 @@
 l1 = ["Eat","Sleep"]
 l2 = ["Apple","Bed"]
 
-zipped = zip(l1,l2)
-print(tuple(zipped))
+# zipped = zip(l1,l2)
+# print(tuple(zipped))
+
+for c ,e in zip(l1,l2):
+    print(c,"is",e)
